42d4191ad62503d54fda14daf51e41eed86e1d88
Ash Rhodes <ashley.rhodes@king.com>
2026-05-12 15:38:48 +0200

    fix: add localhost:5000,5001 to CORS allowed origins for local dev

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5000', 'http://localhost:5001', 'http://localhost:5173', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
